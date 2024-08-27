c038911cf016bec47d8521cbbc9b648c78f2551b
Nick Latcham <nick.latcham@proton.me>
2024-08-27 17:18:49 -0500

    initial commit

---
+++ src/test/java/dev/nicklatcham/shopthing/ShopthingApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nicklatcham.shopthing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopthingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
