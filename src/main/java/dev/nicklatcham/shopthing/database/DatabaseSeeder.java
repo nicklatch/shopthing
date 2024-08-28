8af60a0609bf26819b78ead1d40c096a6238eff4
Nick Latcham <nick.latcham@proton.me>
2024-08-28 15:42:58 -0500

    chore: database seeder

--- src/main/java/dev/nicklatcham/shopthing/database/DatabaseSeeder.java
+++ src/main/java/dev/nicklatcham/shopthing/database/DatabaseSeeder.java
@@ -6,21 +6,26 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import dev.nicklatcham.shopthing.customer.Customer;
+import dev.nicklatcham.shopthing.customer.CustomerRepository;
 import dev.nicklatcham.shopthing.vehicle.Vehicle;
 import dev.nicklatcham.shopthing.vehicle.VehicleRepository;
 
-/**
- * DatabaseSeeder
- */
 @Configuration
 public class DatabaseSeeder {
   private static final Logger log = LoggerFactory.getLogger(DatabaseSeeder.class);
 
   @Bean
-  CommandLineRunner initDatabase(VehicleRepository repo) {
+  CommandLineRunner initDatabase(VehicleRepository vRepo, CustomerRepository cRepo) {
+    // TODO: Set up faker to generate data and persist with batching.
     return args -> {
-      log.info("Seeding: " + repo.save(new Vehicle("Kenworth", "T680", "JKAKLMG181A024994")));
-      log.info("Seeding: " + repo.save(new Vehicle("Kenworth", "T680", "1KAKLMG181HJ35698")));
+      Customer newCust1 = cRepo.save(new Customer("ACME Trucking", "6519996768", "breakdowns@acmetrucking.com"));
+      Customer newCust2 = cRepo.save(new Customer("Other Lines", "7155550978", "info@otherlines.com"));
+
+      log.info("Seeding: " + newCust1);
+      log.info("Seeding: " + newCust2);
+      log.info("Seeding: " + vRepo.save(new Vehicle("Kenworth", "T680", "JKAKLMG181A024994", newCust1)));
+      log.info("Seeding: " + vRepo.save(new Vehicle("Kenworth", "T680", "1KAKLMG181HJ35698", newCust2)));
     };
   }
 }
