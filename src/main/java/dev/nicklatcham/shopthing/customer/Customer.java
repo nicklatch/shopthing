2a1191f959f7f56f16254507c3181d86c4419168
Nick Latcham <nick.latcham@proton.me>
2024-08-28 15:41:49 -0500

    fix: phone regex and json props

--- src/main/java/dev/nicklatcham/shopthing/customer/Customer.java
+++ src/main/java/dev/nicklatcham/shopthing/customer/Customer.java
@@ -1,10 +1,13 @@
 package dev.nicklatcham.shopthing.customer;
 
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import dev.nicklatcham.shopthing.vehicle.Vehicle;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -23,15 +26,15 @@ public class Customer {
   @NotBlank
   private String name;
 
-  // FIXME: Does not allow for country code
-  @Pattern(regexp = "(^$|[0-9]{10})")
+  @Pattern(regexp = "^((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$")
   private String phone;
 
   @Email
   private String email;
 
-  @OneToMany(mappedBy = "customer", cascade = CascadeType.ALL)
-  private List<Vehicle> assets;
+  @OneToMany(fetch = FetchType.EAGER, mappedBy = "customer", orphanRemoval = true)
+  @JsonIgnoreProperties(value = { "customer" }, allowSetters = true)
+  private Set<Vehicle> assets = new HashSet<>();
 
   Customer() {
   }
@@ -74,8 +77,57 @@ public class Customer {
     this.email = email;
   }
 
-  public List<Vehicle> getAssets() {
+  public Set<Vehicle> getAssets() {
     return assets;
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    result = prime * result + ((name == null) ? 0 : name.hashCode());
+    result = prime * result + ((phone == null) ? 0 : phone.hashCode());
+    result = prime * result + ((email == null) ? 0 : email.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Customer other = (Customer) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    if (name == null) {
+      if (other.name != null)
+        return false;
+    } else if (!name.equals(other.name))
+      return false;
+    if (phone == null) {
+      if (other.phone != null)
+        return false;
+    } else if (!phone.equals(other.phone))
+      return false;
+    if (email == null) {
+      if (other.email != null)
+        return false;
+    } else if (!email.equals(other.email))
+      return false;
+    return true;
+  }
+
+  @Override
+  public String toString() {
+    return "Customer [id=" + id + ", name=" + name + ", phone=" + phone + ", email=" + email + ", assets=" + assets
+        + "]";
   }
 
 }
