bfd6256e993469003cd1d43b36d641f9f0447ca6
Nick Latcham <nick.latcham@proton.me>
2024-08-31 20:42:08 -0500

    test: scaffold customer and vehicle class tests

---
+++ src/test/java/dev/nicklatcham/shopthing/customer/CustomerControllerTest.java
@@ -0,0 +1,24 @@
+package dev.nicklatcham.shopthing.customer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerControllerTest {
+
+  @Test
+  void getAllCustomers() {
+  }
+
+  @Test
+  void getCustomerById() {
+  }
+
+  @Test
+  void createCustomer() {
+  }
+
+  @Test
+  void updateCustomer() {
+  }
+}
---
+++ src/test/java/dev/nicklatcham/shopthing/vehicle/VehicleControllerTest.java
@@ -0,0 +1,24 @@
+package dev.nicklatcham.shopthing.vehicle;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VehicleControllerTest {
+
+  @Test
+  void getAllVehicles() {
+  }
+
+  @Test
+  void getVehicleById() {
+  }
+
+  @Test
+  void createVehicle() {
+  }
+
+  @Test
+  void updateVehicleOwner() {
+  }
+}
