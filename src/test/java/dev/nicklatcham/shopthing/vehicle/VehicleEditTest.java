b9a15f737189d91f7e8161427aff3421424beea0
Nick Latcham <nick.latcham@proton.me>
2024-08-31 20:44:43 -0500

    test scaffold

--- src/test/java/dev/nicklatcham/shopthing/vehicle/VehicleEditTest.java
+++ src/test/java/dev/nicklatcham/shopthing/vehicle/VehicleEditTest.java
@@ -10,6 +10,5 @@ public class VehicleEditTest {
   @Test
   void canChangeAddExisitngCustomer() throws Exception {
     // TODO: WRITE THIS TEST
-    Vehicle _ = new Vehicle("Kenworth", "T680", "1XKYD49X3KJ310076");
   }
 }
