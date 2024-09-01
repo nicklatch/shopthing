5f17d2e8d1ff6d053f4aea19f78d07eab66f8bc2
Nick Latcham <nick.latcham@proton.me>
2024-08-31 20:44:02 -0500

    feat: add edit endpoint for put requests

--- src/main/java/dev/nicklatcham/shopthing/customer/CustomerController.java
+++ src/main/java/dev/nicklatcham/shopthing/customer/CustomerController.java
@@ -5,10 +5,10 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +19,7 @@ import jakarta.validation.Valid;
 @RequestMapping("/api/customers")
 public class CustomerController {
   private final CustomerService customerService;
-  private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
+  private static final Logger LOGGER = LoggerFactory.getLogger(CustomerController.class);
 
   public CustomerController(CustomerService customerService) {
     this.customerService = customerService;
@@ -33,14 +33,25 @@ public class CustomerController {
   @GetMapping("/{id}")
   public ResponseEntity<Customer> getCustomerById(@PathVariable Long id) {
     final Customer result = customerService.getCustomerById(id);
-    logger.debug("Result: {}", result);
+    LOGGER.debug("Result: {}", result);
+
     return ResponseEntity.ok(result);
   }
 
   @PostMapping
   public ResponseEntity<?> createCustomer(@Valid @RequestBody Customer newCustomer) {
     final Customer created = customerService.createCustomer(newCustomer);
-    logger.debug("Created Customer: {}", created);
+    LOGGER.debug("Created Customer: {}", created);
+
     return ResponseEntity.status(201).body(created);
   }
+
+  @PutMapping("/{id}/edit")
+  public ResponseEntity<?> updateCustomer(@Valid @RequestBody Customer updatedCustomer) {
+    final Customer updated = customerService.update(updatedCustomer);
+    LOGGER.debug("Updated Customer: {}", updated);
+
+    return ResponseEntity.ok(updated);
+  }
+
 }
