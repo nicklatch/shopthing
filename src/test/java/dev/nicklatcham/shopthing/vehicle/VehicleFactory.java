ceb0a976501e5a2e4d428bcdcce8f73c98455e64
Nick Latcham <nick.latcham@proton.me>
2024-08-31 20:47:39 -0500

    fix: update VehicleFactory with mileage and engineHours params

--- src/test/java/dev/nicklatcham/shopthing/vehicle/VehicleFactory.java
+++ src/test/java/dev/nicklatcham/shopthing/vehicle/VehicleFactory.java
@@ -4,11 +4,13 @@ import dev.nicklatcham.shopthing.customer.Customer;
 
 public class VehicleFactory {
 
-  public static Vehicle withoutCustomer(String make, String model, String vin) {
-    return new Vehicle(make, model, vin);
+  public static Vehicle withoutCustomer(String make, String model, String vin, int odometer,
+      int engineHours) {
+    return new Vehicle(make, model, vin, odometer, engineHours);
   }
 
-  public static Vehicle withCustomer(String make, String model, String vin, Customer customer) {
-    return new Vehicle(make, model, vin, customer);
+  public static Vehicle withCustomer(String make, String model, String vin, int odometer,
+      int engineHours, Customer customer) {
+    return new Vehicle(make, model, vin, odometer, engineHours, customer);
   }
 }
