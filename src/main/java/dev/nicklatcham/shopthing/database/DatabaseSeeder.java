81b148e558b2ead20f1938c148b4e4b798c7e812
Nick Latcham <nick.latcham@proton.me>
2024-08-31 20:41:08 -0500

    chore: add mileage and engine hours to seeder

--- src/main/java/dev/nicklatcham/shopthing/database/DatabaseSeeder.java
+++ src/main/java/dev/nicklatcham/shopthing/database/DatabaseSeeder.java
@@ -13,19 +13,24 @@ import dev.nicklatcham.shopthing.vehicle.VehicleRepository;
 
 @Configuration
 public class DatabaseSeeder {
+
   private static final Logger log = LoggerFactory.getLogger(DatabaseSeeder.class);
 
   @Bean
   CommandLineRunner initDatabase(VehicleRepository vRepo, CustomerRepository cRepo) {
     // TODO: Set up faker to generate data and persist with batching.
     return args -> {
-      Customer newCust1 = cRepo.save(new Customer("ACME Trucking", "6519996768", "breakdowns@acmetrucking.com"));
-      Customer newCust2 = cRepo.save(new Customer("Other Lines", "7155550978", "info@otherlines.com"));
+      Customer newCust1 = cRepo.save(
+          new Customer("ACME Trucking", "6519996768", "breakdowns@acmetrucking.com"));
+      Customer newCust2 = cRepo.save(
+          new Customer("Other Lines", "7155550978", "info@otherlines.com"));
 
-      log.info("Seeding: " + newCust1);
-      log.info("Seeding: " + newCust2);
-      log.info("Seeding: " + vRepo.save(new Vehicle("Kenworth", "T680", "JKAKLMG181A024994", newCust1)));
-      log.info("Seeding: " + vRepo.save(new Vehicle("Kenworth", "T680", "1KAKLMG181HJ35698", newCust2)));
+      log.info("Seeding: {}", newCust1);
+      log.info("Seeding: {}", newCust2);
+      log.info(STR."Seeding: \{vRepo.save(
+          new Vehicle("Kenworth", "T680", "JKAKLMG181A024994", 734_345, 12_00, newCust1))}");
+      log.info(STR."Seeding: \{vRepo.save(
+          new Vehicle("Kenworth", "T680", "1KAKLMG181HJ35698", 987_102, 13_099, newCust2))}");
     };
   }
 }
